38c824da846c420cf913530ae24af8b62d7509bb
ZviksZ <viks2332@gmail.com>
2018-08-23 13:56:46 +0300

    first commit

---
+++ Redsoft/js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+
+	$(".header__nav-btn").click(function(){
+		$(".header__nav").slideToggle(500)
+	})
+
+});
---
+++ Template/js/main.js
@@ -0,0 +1,9 @@
+
+$(function(){
+		$('.header-menu').slicknav({
+			prependTo: ".container-menu"		
+		})
+	});
+
+
+	
