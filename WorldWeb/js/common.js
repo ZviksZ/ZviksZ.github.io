01ff6e0dc520018c8a5b2f4d3368f34a736c1063
ZviksZ <viks2332@gmail.com>
2018-08-31 23:56:30 +0300

    Portfolio

---
+++ WorldWeb/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider__wrap').slick({
+  		dots: true
+  });
+});
+
+$(function(){
+	$('.top-nav__menu-responsive').slicknav({
+		label: '',
+		prependTo: ".top-nav .container"
+	});
+});
